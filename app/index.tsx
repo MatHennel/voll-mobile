03784ca4189e9ae692b9e6afd74585e3f9a8a6e4
MatHennel <matheushennel123@gmail.com>
2024-06-24 02:02:16 -0300

    criando tela principal

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,8 @@
+import Principal from "@/src/Principal";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Principal/>
   );
 }
